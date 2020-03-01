7b740b77a73bd062f5c4245db1d985ce6285fb64
epanlauw <epanlauw6@gmail.com>
2020-03-01 20:10:15 +0700

    versi 0.0.3

---
+++ View/signup.php
@@ -0,0 +1,12 @@
+<!DOCType html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.min.js"></script>
+        <title>Sign Up - Untuk Pendaftaran</title>
+    </head>
+    <body>
+
+    </body>
+</html>
