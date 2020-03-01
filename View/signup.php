ab85e664222692fe7e9af13d2594c82160849f43
epanlauw <epanlauw6@gmail.com>
2020-03-01 21:09:21 +0700

    versi 0.0.4.2

--- View/signup.php
+++ View/signup.php
@@ -1,12 +1,33 @@
 <!DOCType html>
 <html>
     <head>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <title>Sign Up - Untuk Pendaftaran</title>
     </head>
     <body>
-
+        <div class="container">
+            <h2>CREATE NEW ACCOUNT</h2>
+            <h5>It's free and always be.</h5>
+            <form action="">
+                <div class="form-group">
+                    <div class="col-md-4">
+                        <input type="text" name="first_name" class="form-control" placeholder="First Name">
+                    </div>
+                    <div class="col-md-4">
+                        <input type="text" name="last_name" class="form-control" placeholder="Last Name">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-8">
+                        <br/>
+                        <input type="text" name="email" class="form-control" placeholder="Email">
+                    </div>
+                </div>
+            </form>
+        </div>
     </body>
 </html>
