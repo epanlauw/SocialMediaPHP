132b51df5fe2eeb7dd70b7962c72f2becd8e45cc
epanlauw <epanlauw6@gmail.com>
2020-03-01 23:17:59 +0700

    versi 0.0.4.4

--- View/signup.php
+++ View/signup.php
@@ -7,12 +7,24 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <title>Sign Up - Untuk Pendaftaran</title>
+
+        <script>
+            function validate() {
+                var a = document.getElementById("password").value;
+                var b = document.getElementById("confirm-password").value;
+
+                if(a != b){
+                    alert("Password do not match");
+                    return false;
+                }
+            }
+        </script>
     </head>
     <body>
         <div class="container">
             <h2>CREATE NEW ACCOUNT</h2>
             <h5>It's free and always be.</h5>
-            <form action="">
+            <form action="" method="post" onSubmit="return validate();">
                 <div class="form-group">
                     <div class="col-md-4">
                         <input type="text" name="first_name" class="form-control" placeholder="First Name">
@@ -27,7 +39,21 @@
                         <input type="text" name="email" class="form-control" placeholder="Email">
                     </div>
                 </div>
+                <div class="form-group">
+                    <div class="col-md-8">
+                        <br/>
+                        <input type="password" name="password" class="form-control" placeholder="Password" id="password">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-8">
+                        <br/>
+                        <input type="password" name="password" class="form-control" placeholder="Confirmation Password" id="confirm-password">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary" name="submit">Sign Up</button>
             </form>
         </div>
+
     </body>
 </html>
