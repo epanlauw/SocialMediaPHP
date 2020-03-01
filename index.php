953e2a3ebfd720774fa62755b783d2469f1c24cd
epanlauw <epanlauw6@gmail.com>
2020-03-01 22:33:33 +0700

    versi 0.0.4.3

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie-edge">
         <title>Sosmed</title>
-
+        <!--Import!-->
         <link rel="stylesheet" href="css/bootstrap.min.css"/>
     </head>
     <body class="bg-light">
