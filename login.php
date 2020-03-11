bbe74f380bbb6204196336f9bcc7fc803adf8f5a
epanlauw <epanlauw6@gmail.com>
2020-03-11 22:32:22 +0700

    versi 0.0.5.2.0

--- login.php
+++ login.php
@@ -1,65 +1,98 @@
 <?php 
     require_once("config.php");
-    if(isset($_POST['login'])){      
-        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-        $password = $_POST['password'];
-
-        $login = mysqli_query($db,"select * from users where email = '$email' and password = MD5(CONCAT('$password',users.salt))");
-
-        $query = "select * from users where email = '$email' and password = MD5(CONCAT('$password',users.salt))";
-        
-        $cek = mysqli_num_rows($login);
-        $result = $db->query($query);
-        $row = $result -> fetch_assoc();
-        if($cek >0){
-            session_start();
-            $_SESSION['user'] = $row;
-            $_SESSION['status'] = $login;
-            header("Location:timeline.php");
+    
+    if(isset($_POST['login'])){   
+        session_start();
+        if($_SESSION["code"] != $_POST["kodecaptcha"]){
+            echo "Kode Captcha Salah";
         }else{
-            echo "false";
-        }
+            session_unset("code");
+            $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+            $password = $_POST['password'];
+
+            $login = mysqli_query($db,"select * from users where email = '$email' and password = MD5(CONCAT('$password',users.salt))");
+
+            $query = "select * from users where email = '$email' and password = MD5(CONCAT('$password',users.salt))";
+            
+            $cek = mysqli_num_rows($login);
+            $result = $db->query($query);
+            $row = $result -> fetch_assoc();
+            if($cek >0){
+                session_start();
+                $_SESSION['user'] = $row;
+                $_SESSION['status'] = $login;
+                header("Location:timeline.php");
+            }else{
+                echo "false";
+            }
+        }   
+        
     }
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        <title>Login Cuy</title>
-    </head>
-    <body class="bg-light">
-        <div class="container mt-5">
-        <div class="row">
-            <p>&larr; <a href="index.php">Home</a>
-            <h4>Masuk ke Sosmed</h4>
-            <p>Belum punya akun?<a href="signup.php">Registration</a>
-
-            <form action="" method="POST">
-                <div class="form-group">
-                    <div class="col-md-8">
-                        <label for="email">Email</label>
-                        <input type="text" class="form-control" name="email" placeholder="Email"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-md-8">
-                        <br/>
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" name="password" placeholder="Password"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-md-8">
-                    <br />
-                    <input type="submit" class="btn btn-success btn-block" name="login" value="Masuk"/>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </body>
+    
+<head>
+	<title>Login Page</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="container h-100">
+		<div class="d-flex justify-content-center h-100">
+			<div class="user_card">
+				<div class="d-flex justify-content-center">
+					<div class="brand_logo_container">
+						<img src="https://cdn4.iconfinder.com/data/icons/animals-45/755/Giraffe-512.png" width="200px" height="200px" class="brand_logo" alt="Logo">
+					</div>
+				</div>
+				</br>
+				<h3> USER LOGIN </h3>
+				<div class="d-flex justify-content-center form_container">
+					<form action="" method="POST" >
+						<div class="input-group mb-3">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-user"></i></span>
+							</div>
+							<input type="text" name="email" class="form-control input_user" value="" placeholder="username">
+						</div>
+						<div class="input-group mb-2">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-key"></i></span>
+							</div>
+							<input type="password" name="password" class="form-control input_pass" value="" placeholder="password">
+						</div>
+						<div class="d-flex justify-content-center">
+								<img src="captcha.php" alt="gambar">
+						</div>
+						<div class="input-group mb-2">
+							<input name="kodecaptcha" class="form-control input_captcha" value="" placeholder="captcha" maxlength="5">
+						</div>
+						<div class="form-group">
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" class="custom-control-input" id="customControlInline">
+								<label class="custom-control-label" for="customControlInline">Remember me</label>
+							</div>
+						</div>
+							<div class="d-flex justify-content-center mt-3 login_container">
+				 	    <input type="submit" class="btn btn-success btn-block" name="login" value="Masuk"/>
+				   </div>
+					</form>
+				</div>
+		
+				<div class="mt-2">
+					<div class="d-flex justify-content-center links">
+						Don't have an account? <a href="signup.php" class="ml-2">Sign Up</a>
+					</div>
+					<div class="d-flex justify-content-center links">
+						<a href="#">Forgot your password?</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
 </html>
